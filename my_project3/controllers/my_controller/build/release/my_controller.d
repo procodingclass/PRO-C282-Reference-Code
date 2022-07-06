c8fd18defcc7ed0d36e452ca15863d0859ec0dc9
procodingclass <88050205+procodingclass@users.noreply.github.com>
2022-07-06 23:46:53 +0530

    Delete my_project3 directory

--- my_project3/controllers/my_controller/build/release/my_controller.d
+++
@@ -1,7 +0,0 @@
-build/release/my_controller.o: my_controller.c \
- C:\Program\ Files\Webots/include/controller/c/webots/altimeter.h \
- C:\Program\ Files\Webots/include/controller/c/webots/types.h \
- C:\Program\ Files\Webots/include/controller/c/webots/led.h \
- C:\Program\ Files\Webots/include/controller/c/webots/motor.h \
- C:\Program\ Files\Webots/include/controller/c/webots/robot.h \
- C:\Program\ Files\Webots/include/controller/c/webots/nodes.h
